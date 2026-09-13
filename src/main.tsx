680e531d52dde3f94e77ff6eaec56500040ebc9a
Ann00012 <ann.malchevska@gmail.com>
2026-09-13 12:21:01 +0300

    feat:set up React Router and add ui-library showcase route

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import {BrowserRouter, Routes, Route} from 'react-router';
+import App from './App';
+import StoryPoint from './pages/StoryPoint/index';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-
-  </StrictMode>,
- 
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/ui-library" element={<StoryPoint />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
