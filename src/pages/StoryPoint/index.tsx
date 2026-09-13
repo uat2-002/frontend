8cd3a31a0e8365d6754c3abe0209a45cc80d5413
Ann00012 <ann.malchevska@gmail.com>
2026-09-13 12:17:46 +0300

    feat: add ButtonComponent showcase

---
+++ src/pages/StoryPoint/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ButtomComponent from './ButtonComponent';
+const StoryPoint = () => {
+  return (
+    <>
+      <ButtomComponent />
+    </>
+  );
+};
+
+export default StoryPoint;
