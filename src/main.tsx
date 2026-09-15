7043c33b4bf52983028ec042de832fc3dcd5b6e5
AnnaMalchevska <ann.malchevska@gmail.com>
2026-09-15 08:58:47 +0300

    EP0-5-add-react-router (#6)
    * feat: install react-router
    
    * feat:add App.tsx file
    
    * feat: add ButtonComponent showcase
    
    * feat:set up React Router and add ui-library showcase route

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import {createBrowserRouter, RouterProvider} from 'react-router';
+import App from './App';
+import StoryBook from './pages/StoryBook/index';
+import './index.css';
+
+const router = createBrowserRouter([
+  {path: '/', element: <App />},
+  {path: '/ui-library', element: <StoryBook />},
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-
-  </StrictMode>,
-)
+   <RouterProvider router={router} />
+  </StrictMode>
+);
