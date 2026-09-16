946d692b14a0d0f8e57279ce7e28f97b8a01bf2f
Ann00012 <ann.malchevska@gmail.com>
2026-09-16 20:29:40 +0300

    feat: add handle unknown routes with a 404 page

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,9 +1,11 @@
 import { Home } from '@/pages/Home';
 import StoryBook from '@/pages/StoryBook/index';
+import NotFound from '@/components/ui/notFound';
 import { createBrowserRouter } from 'react-router';
-import { PATH_HOME, PATH_UI_LIBRARY } from './path';
+import { PATH_HOME, PATH_UI_LIBRARY, PATH_NOT_FOUND } from './path';
 
 export const router = createBrowserRouter([
   { path: PATH_HOME, element: <Home /> },
   { path: PATH_UI_LIBRARY, element: <StoryBook /> },
+  { path: PATH_NOT_FOUND, element: <NotFound /> },
 ]);
