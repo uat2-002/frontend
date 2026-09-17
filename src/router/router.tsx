9204bb07fefb968c718c0f421fc46ed8ee6bc2f6
Ann00012 <ann.malchevska@gmail.com>
2026-09-17 10:11:58 +0300

    feat: setup ToastContainer in App and configure path aliases

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,7 +1,7 @@
 import { Home } from '@/pages/Home';
 import StoryBook from '@/pages/StoryBook/index';
 import { createBrowserRouter } from 'react-router';
-import { PATH_HOME, PATH_UI_LIBRARY } from './path';
+import { PATH_HOME, PATH_UI_LIBRARY } from '@/router/path';
 
 export const router = createBrowserRouter([
   { path: PATH_HOME, element: <Home /> },
