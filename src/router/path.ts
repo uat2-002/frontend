6a9f74d7931f6f341c4bcef4d49a292e70a40e94
Oleksii Strotskyi <strotskyi.oleksii@gmail.com>
2026-09-18 16:37:38 +0300

    feat: change router

--- src/router/path.ts
+++ src/router/path.ts
@@ -1,3 +1,4 @@
 export const PATH_HOME = '/';
 export const PATH_UI_LIBRARY = '/ui-library';
+export const PATH_MY_LIST = '/my_list';
 export const PATH_NOT_FOUND = '/*';
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,10 +2,19 @@ import { Home } from '@/pages/Home';
 import StoryBook from '@/pages/StoryBook/index';
 import NotFound from '@/pages/NotFound';
 import { createBrowserRouter } from 'react-router';
-import { PATH_HOME, PATH_UI_LIBRARY,PATH_NOT_FOUND } from '@/router/path';
+import AppLayout from '@/components/shared/AppLayout';
+import { PATH_HOME, PATH_UI_LIBRARY, PATH_MY_LIST, PATH_NOT_FOUND } from '@/router/path';
+import { MyList } from '@/pages/MyList';
 
 export const router = createBrowserRouter([
-  { path: PATH_HOME, element: <Home /> },
+  {
+    element: <AppLayout />,
+    children: [
+      { path: PATH_HOME, element: <Home /> },
+      { path: PATH_MY_LIST, element: <MyList /> },
+    ],
+
+  },
   { path: PATH_UI_LIBRARY, element: <StoryBook /> },
   { path: PATH_NOT_FOUND, element: <NotFound /> },
 ]);
