25f8e93070df81b315375feb11fb26f23b1e9712
OleksiiStrotskyi <strotskyi.oleksii@gmail.com>
2026-09-19 16:59:27 +0300

    Feature/fe0-3: app layout and header (#13)
    * feat: add user avatar
    
    * feat: add header and app layout
    
    * feat: add initial my list page
    
    * feat: change router
    
    * fix: paths work automatically
    
    * fix: do cleaning
    
    * fix: separete navigation list
    
    * fix: split header components
    
    * fix: resolve merge conflicts with development

--- src/router/path.ts
+++ src/router/path.ts
@@ -1,5 +1,5 @@
-export const PATH_HOME = '/';
-export const PATH_UI_LIBRARY = '/ui-library';
+export const PATH_HOME: string = '/';
+export const PATH_UI_LIBRARY: string = '/ui-library';
 export const PATH_SIGN_UP = '/sign-up';
-export const PATH_NOT_FOUND = '/*';
-
+export const PATH_MY_LIST: string = '/my-list';
+export const PATH_NOT_FOUND: string = '/*';
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,13 +1,21 @@
 import { Home } from '@/pages/Home';
 import StoryBook from '@/pages/StoryBook/index';
-import SignUpPage from '@/pages/SignUp/index';
 import NotFound from '@/pages/NotFound';
 import { createBrowserRouter } from 'react-router';
-import { PATH_HOME, PATH_UI_LIBRARY, PATH_SIGN_UP, PATH_NOT_FOUND } from '@/router/path';
+import AppLayout from '@/components/shared/AppLayout';
+import { PATH_HOME, PATH_UI_LIBRARY, PATH_MY_LIST, PATH_SIGN_UP, PATH_NOT_FOUND } from '@/router/path';
+import SignUpPage from '@/pages/SignUp/index';
+import { MyList } from '@/pages/MyList';
 
 export const router = createBrowserRouter([
-  { path: PATH_HOME, element: <Home /> },
-  { path: PATH_UI_LIBRARY, element: <StoryBook /> },
+  {
+    element: <AppLayout />,
+    children: [
+      { path: PATH_HOME, element: <Home /> },
+      { path: PATH_MY_LIST, element: <MyList /> },
+    ],
+  },
   { path: PATH_SIGN_UP, element: <SignUpPage /> },
+  { path: PATH_UI_LIBRARY, element: <StoryBook /> },
   { path: PATH_NOT_FOUND, element: <NotFound /> },
 ]);
