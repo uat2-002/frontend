c0ec1d97ba58a9d064f38b98614f1bff15357f5c
Sofiia <sofiiamal2710@gmail.com>
2026-09-24 18:55:34 +0300

    fix: resolved conflict with router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,23 +1,41 @@
+import { AppLayout } from '@/components/AppLayout';
+import type { AuthUser } from '@/components/ProtectedRoutes/ProtectedRoutes';
+import { ProtectedRoutes } from '@/components/ProtectedRoutes/ProtectedRoutes';
 import { Home } from '@/pages/Home';
-import StoryBook from '@/pages/StoryBook/index';
-import NotFound from '@/pages/NotFound';
-import { createBrowserRouter } from 'react-router';
-import AppLayout from '@/components/shared/AppLayout';
-import { PATH_HOME, PATH_UI_LIBRARY, PATH_MY_LIST, PATH_SIGN_UP, PATH_BROWSE, PATH_NOT_FOUND } from '@/router/path';
-import SignUpPage from '@/pages/SignUp/index';
-import { MyList } from '@/pages/MyList';
+import { NotFound } from '@/pages/NotFound';
+import { SeriesList } from '@/pages/SeriesList';
+import { SignIn } from '@/pages/SignIn';
+import { SignUp } from '@/pages/SignUp';
 import { BrowsePage } from '@/pages/Browse';
+import UiLibrary from '@/pages/UiLibrary';
+import {
+  PATH_HOME,
+  PATH_MY_LIST,
+  PATH_NOT_FOUND,
+  PATH_SIGN_IN,
+  PATH_SIGN_UP,
+  PATH_UI_LIBRARY,
+  PATH_BROWSE,
+} from '@/router/path';
+import { createBrowserRouter } from 'react-router';
+
+// mocked: add user for testing here
+const user: AuthUser | null = null;
 
 export const router = createBrowserRouter([
   {
     element: <AppLayout />,
     children: [
+      {
+        element: <ProtectedRoutes user={user} />,
+        children: [{ path: PATH_MY_LIST, element: <SeriesList /> }],
+      },
       { path: PATH_HOME, element: <Home /> },
-      { path: PATH_MY_LIST, element: <MyList /> },
+      { path: PATH_UI_LIBRARY, element: <UiLibrary /> },
       { path: PATH_BROWSE, element: <BrowsePage /> },
+      { path: PATH_NOT_FOUND, element: <NotFound /> },
     ],
   },
-  { path: PATH_SIGN_UP, element: <SignUpPage /> },
-  { path: PATH_UI_LIBRARY, element: <StoryBook /> },
-  { path: PATH_NOT_FOUND, element: <NotFound /> },
+  { path: PATH_SIGN_UP, element: <SignUp /> },
+  { path: PATH_SIGN_IN, element: <SignIn /> },
 ]);
